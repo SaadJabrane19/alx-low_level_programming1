b8c6c163dee4839658110bf713598d112f81e555
SaadJabrane19 <saadjabrane130@gmail.com>
2023-03-17 08:04:39 -0700

    size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using printf 
- * Return : 0 (success)
+ * main - A program that print a line 
+ * Return (0)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+	printf(with proper grammar, but the outcome is a piece of art,\n);
+	return 0;
 }
